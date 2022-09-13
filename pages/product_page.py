44f2dbbde0496e7c79e550fb8ef166ae5a01bb42
QuaSanse <quasanse@gmail.com>
2022-09-13 20:46:35 +0300

    Add pytest.ini for marks, update product_page, test

--- pages/product_page.py
+++ pages/product_page.py
@@ -15,7 +15,7 @@ class ProductPage(BasePage):
 
     def should_be_product_url(self):
         """ проверка на верный url адрес """
-        assert '?promo=newYear' in self.browser.current_url, "Current url does not contain product page"
+        assert '?promo=' in self.browser.current_url, "Current url does not contain product page"
 
     def go_add_product_to_basket(self):
         """ метод добавления товара в корзину """
@@ -38,8 +38,8 @@ class ProductPage(BasePage):
             *ProductPageLocators.PRODUCT_NAME_IN_MESSEGE)
 
         print(
-            f'Товар "{product_name_on_page.text}" добавлен в корзину')
-        assert product_name_on_page.text == product_name_in_messege.text, "Product name does not match"
+            f'Товар "{product_name_in_messege.text}" добавлен в корзину')
+        assert product_name_on_page.text == product_name_in_messege.text, f"Product name {product_name_on_page.text} does not match {product_name_in_messege.text}"
 
     def check_price_message(self):
         """ метод сравнения цен со страницы и после добавления """
@@ -51,3 +51,13 @@ class ProductPage(BasePage):
         assert price_on_page.text == price_on_messege.text, "Prices do not match"
 
         print(f'Стоимость корзины {price_on_messege.text}')
+
+    def should_not_be_success_message(self):
+        """ проверяет, что элемент не появляется на странице в течение заданного времени, если элемент находиться , тест падает """
+        assert self.is_not_element_present(*ProductPageLocators.PRICE_PRODUCT_IN_MESSEGE), \
+            "Success message is presented, but should not be"
+
+    def the_success_message_should_disappear(self):
+        """ проверяет, что элемент не появляется на странице в течение заданного времени, если элемент находиться , тест падает """
+        assert self.is_disappeared(*ProductPageLocators.PRICE_PRODUCT_IN_MESSEGE), \
+            "Success message should disappear, but didn't"
--- test_product_page.py
+++ test_product_page.py
@@ -1,10 +1,57 @@
+import time
 from .pages.main_page import MainPage
 from .pages.product_page import ProductPage
+import pytest
+
+product_link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo="
 
 
-def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
-    page = MainPage(browser, link)
+@pytest.mark.parametrize('promo_offer',
+                         [pytest.param(i, marks=pytest.mark.xfail(i == 7, reason='fixing this bug right now')) for i in range(10)])
+def test_guest_can_add_product_to_basket(browser, promo_offer):
+    url = f"{product_link}offer{promo_offer}"
+    page = MainPage(browser, url)
     page.open()
     product_page = ProductPage(browser, browser.current_url)
     product_page.should_be_product_page()
+    # time.sleep(15)
+
+
+@pytest.mark.skip(reason="no way of currently testing this")
+@pytest.mark.smoke
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    pass
+    # Открываем страницу товара
+    page = MainPage(browser, product_link)
+    page.open()
+    # Добавляем товар в корзину
+    product_page = ProductPage(browser, browser.current_url)
+    product_page.go_add_product_to_basket()
+    # Проверяем, что нет сообщения об успехе с помощью is_not_element_present
+    product_page.should_not_be_success_message()
+
+
+@pytest.mark.skip(reason="no way of currently testing this")
+@pytest.mark.smoke
+def test_guest_cant_see_success_message(browser):
+    pass
+    # Открываем страницу товара
+    page = MainPage(browser, product_link)
+    page.open()
+    # Проверяем, что нет сообщения об успехе с помощью is_not_element_present
+    product_page = ProductPage(browser, browser.current_url)
+    product_page.should_not_be_success_message()
+
+
+@pytest.mark.skip(reason="no way of currently testing this")
+@pytest.mark.smoke
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    pass
+    # Открываем страницу товара
+    page = MainPage(browser, product_link)
+    page.open()
+    # Добавляем товар в корзину
+    product_page = ProductPage(browser, browser.current_url)
+    product_page.go_add_product_to_basket()
+    # Проверяем, что нет сообщения об успехе с помощью is_disappeared
+    product_page.the_success_message_should_disappear()
