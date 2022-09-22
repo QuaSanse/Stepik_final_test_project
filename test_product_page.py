9d68692b95a365c420893ed0f13edbc017b399b4
QuaSanse <quasanse@gmail.com>
2022-09-22 20:41:38 +0300

    Add test_product_page go_to_cart

--- test_product_page.py
+++ test_product_page.py
@@ -1,6 +1,7 @@
 import time
 from .pages.main_page import MainPage
 from .pages.product_page import ProductPage
+from .pages.basket_page import BasketPage
 import pytest
 
 product_link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo="
@@ -58,6 +59,7 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     product_page.the_success_message_should_disappear()
 
 
+@pytest.mark.skip(reason="no way of currently testing this")
 def test_guest_should_see_login_link_on_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
@@ -65,8 +67,23 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.should_be_login_link()
 
 
+@pytest.mark.skip(reason="no way of currently testing this")
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = ProductPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    pass
+    # Гость открывает страницу товара
+    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    # Переходит в корзину по кнопке в шапке
+    page.go_to_cart()
+    # Ожидаем, что в корзине нет товаров
+    # Ожидаем, что есть текст о том что корзина пуста
+    basket_page = BasketPage(browser, browser.current_url)
+    basket_page.should_no_items_in_the_cart()
